d2108212b9083494b6a129c7d0345237f2d83e10
dewan765 <dewansadik41@gmail.com>
2025-11-17 13:47:40 -0500

    All the code that I didn't add last time and new code.

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,6 @@
-const express = 
-require('express');
+const express = require('express');
 
-const router = 
-express.Router();
+const router = express.Router();
 
 router.get('/', (req, res)=>{
     res.send('User List');
@@ -10,5 +8,26 @@ router.get('/', (req, res)=>{
 router.get('/new', (req, res)=>{
     res.send('New User Form');
 });
+router.post('/', (req, res)=> {
 
+});
+
+/*
+router.get(':/id', (req, res)=> {
+    res.send(`Getting User data: ${req.params.id}`);
+});*/
+
+router.route("/:id").get((req,res)=> {
+    res.send(`Getting User data: ${req.params.id}`);
+}).delete((req, res)=>{
+    res.send(`Deleting user with id: ${req.params.id}`);
+}).put((req,res)=>{
+    res.send(`Updating user with id: ${req.params.id}`);
+});
+const users = [{name:"George"},{name:"Justyna"}];
+
+router.param,("id", (req, res, next, id)=>{
+    console.log(`Accessing user #${id}`);
+    next();
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,30 @@
-const express = 
-require('express');
+const express = require('express');
 
-const userRouter = 
-require('./routes/users');
+const userRouter = require('./routes/users');
 
-const postRouter =
-require('./routes/post');
+const postRouter = require('./routes/post');
 
 const app = express(); // Calling this function sets up a server
 app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+//app.use(logger);
+
+app.use('/users', userRouter);
+app.use('/posts', postRouter);
 
 app.get('/', (req, res) => {
     console.log('Here');
-    res.render("index", {user:"George!",});
+    res.render("index", 
+        {user:"George!",});
 });
-app.get('/users', (req, res)=>{
-    res.send('User List');
-});
-app.get('/post', (req,res)=>{
-    res.send('Post Page');
-});
+app.get('/new', (req,res)=>{
+    res.render('users/new',{firstName: "Please enter your name"});
+})
 
 app.listen(3030); 
+
+function logger(req, res, next){
+    console.log(`Page Accessed: ${req.originalUrl}`);
+    next();
+}
